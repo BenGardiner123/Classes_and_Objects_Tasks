4ce0a8f964db0b742ee2bbc8c4ad7179179dfd5d
Ben Gardiner <bennygards@gmail.com>
2020-05-05 20:54:23 +1000

    Task 1.1 and 1.2

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Classes_and_Objects_Tasks
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ///this code instantiates a cookie
+            Cookie c1 = new Cookie();
+        } 
+    }
+
+    /////this is my basic cookie class
+    class Cookie
+    {
+        //attributees
+        public int Weight;
+        public string Shape;
+
+        //operations
+
+        /////=====================
+        //constructor
+
+        public Cookie(){
+            this.Weight = 0;
+            this.Shape = "";
+
+        }
+
+         //operations
+
+    }
+
+     /*    class Colour{
+
+            int cookieColour
+
+            /////=====================
+            //constructor
+
+            public Colour(){
+                cookieColour = 255 204 0;
+            }
+ */
+    
+
+} 
