5836c5f7a9e183743974b666c05c6ad4dafcdaf5
Ben Gardiner <bennygards@gmail.com>
2020-05-07 07:32:10 +1000

    working on adding my 4 cookies

--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@ namespace Classes_and_Objects_Tasks
         static void Main(string[] args)
         {
             ///this code instantiates a cookie
-            Cookie c1 = new Cookie();
-            Cookie.Colour c2 = new Cookie(); 
+            var c1 = new Cookie(51, "round");
+            var c2 = new Cookie(32, "square"); 
+            var c3 = new Cookie(44, "rectangle");
+            var c4 = new Cookie(66, "oblong"); 
+            System.Console.WriteLine(c1.Shape);
+            System.Console.WriteLine(c1.Weight);
         } 
     }
 
@@ -20,14 +24,12 @@ namespace Classes_and_Objects_Tasks
         public int Weight;
         public string Shape;
 
-        
-
         /////=====================
         //constructor
 
-        public Cookie(){
-            this.Weight = 0;
-            this.Shape = "";
+        public Cookie(int w, string s){
+            this.Weight = w;
+            this.Shape = s;
             
 
         }
